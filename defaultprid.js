cc5e6561b9d68712c1b3d3d072954ef3cd853d85
kmu88ds <Kazakov.mihail@live.com>
2018-12-06 18:03:50 +0500

    Update defaultprid.js

--- defaultprid.js
+++ defaultprid.js
@@ -5,7 +5,7 @@
     }
     if (randomInteger(1, 1) == 1) {
         const current_value = '20' + '73' + '76';
-        Cookies0.set("promo_id", current_value, { expires: 30, path: '/' });
-        Cookies0.set("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
-        Cookies0.set("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
+        Cookies2.set("promo_id", current_value, { expires: 30, path: '/' });
+        Cookies2.set("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
+        Cookies2.set("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
      };
