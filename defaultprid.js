fc58721ac6a36bc2b9e8bf4555990f38ddcea913
kmu88ds <Kazakov.mihail@live.com>
2018-12-06 18:17:53 +0500

    Update defaultprid.js

--- defaultprid.js
+++ defaultprid.js
@@ -6,8 +6,8 @@ function randomInteger(min, max) {
         rand = Math.floor(rand);
         return rand;
     };
+const current_value = '20' + '73' + '76';
     if (randomInteger(1, 1) == 1) {
-        const current_value = '20' + '73' + '76';
         Cookies2.set("promo_id", current_value, { expires: 30, path: '/' });
         Cookies2.set("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
         Cookies2.set("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
