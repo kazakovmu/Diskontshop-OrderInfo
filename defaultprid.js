b1d242a2be02cdf921a56f1ac87943e17343d0ee
kmu88ds <Kazakov.mihail@live.com>
2019-02-05 00:22:28 +0500

    Update defaultprid.js

--- defaultprid.js
+++ defaultprid.js
@@ -5,7 +5,7 @@ function randomInteger(min, max) {
         return rand;
     };
 var Cookies0 = Cookies.noConflict();
-    if (randomInteger(1, 70) == 25) {
+    if (randomInteger(1, 120) == 25) {
         Cookies0.set("promo_id", current_value, { expires: 30, path: '/' });
         Cookies0.set("referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
         Cookies0.set("promo_referer", "http%3A%2F%2Faway.vk.com%2Faway.php", { expires: 30, path: '/' });
